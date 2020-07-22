8ecf1ef84453b785ac3168990820c213f52f2264
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2020-07-21 23:52:30 -0300

    Adição de mais funcionalidades ao código.

--- parametros_mensais.py
+++ parametros_mensais.py
@@ -4,10 +4,35 @@ from dateutil.relativedelta import relativedelta
 import smtplib
 from email.message import EmailMessage
 from locale import setlocale, LC_ALL
+import assists
+from enum import Enum
+
+class LimiteAnswerPergunta(Enum):
+    Cgv = 0
+    N4 = 1
+    Avulso = 2
 
 class Parametros:
     def __init__(self):
         self.montante = int
+
+    def interface_client(self):
+        self.pergunta()
+        self.montante = input('Qual o valor do parâmetro? ')
+
+    def pergunta (self):
+        self.ask = input("Qual tipo de contrato irá fazer o cadastro? ").title()
+        if not isinstance(self.ask, LimiteAnswerPergunta):
+            raise ValueError('Contrato não cadastrado.')
+
+        return f'{self.ask}
+
+
+    def abrir_arq(self):
+        pass
+
+    def save_arq(self):
+        pass
 
     @staticmethod
     def marca():
@@ -19,7 +44,7 @@ class Parametros:
 
     @staticmethod
     def orgv():
-        return 1001
+        return "1001"
 
     def tipo_contrato(self):
         pass
@@ -47,10 +72,13 @@ class Parametros:
         return "M20"
 
     def tab(self):
-        pass
+        tabela = {'Cgv':689, 'Avulsos':525}
+        if self.answer_pergunta() in tabela.keys():
+            return tabela.values()
 
-    def data_inicio(self):
-        pass
+    @staticmethod
+    def data_inicio():
+        return assists.data_inicio()
 
     def data_fim(self):
         pass
