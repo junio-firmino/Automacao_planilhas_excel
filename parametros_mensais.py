4c2de3392710e4b6e4a61196a3f5cb188b9af1e6
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2020-07-22 21:21:35 -0300

    Mais funcionalidades ao código.

--- parametros_mensais.py
+++ parametros_mensais.py
@@ -21,12 +21,32 @@ class Parametros:
         self.montante = input('Qual o valor do parâmetro? ')
 
     def pergunta (self):
-        self.ask = input("Qual tipo de contrato irá fazer o cadastro? ").title()
+        self.ask = input("Escolha o tipo de contrato? ").title()
         if not isinstance(self.ask, LimiteAnswerPergunta):
             raise ValueError('Contrato não cadastrado.')
 
-        return f'{self.ask}
+        return self.ask
 
+    def cliente_centro_produto(self):
+        self.filiais()[0] = {'PB.620': [1100, 1101, 1150, 1160]}
+        self.filiais()[1] = {'PB.658': [1100, 1101, 1160],'PB.620': [1100, 1101], 'PB,6DH': 1160}
+        self.filiais()[2] = {'PB.620': 1100, 'PB.658': 1100}
+        self.filiais()[3] = {'PB.620': 1101, 'PB.658': 1101}
+        self.filiais()[4] = {'PB.620': 1111, 'PB.658': 1111}
+        self.filiais()[5] = {'PB.620': 1120}
+        self.filiais()[6] = {'PB.620': 1120, 'PB.658': 1120, 'PB.6DH': 1120}
+        self.filiais()[7] = {'PB.620': 1120, 'PB.658': 1120, 'PB.6DH': 1120}
+        self.filiais()[8] = {'PB.620': 1150}
+        self.filiais()[9] = {'PB.620': 1150, 'PB.658': 1150, 'PB.6DH': 1150}
+        self.filiais()[10] = {'PB.620': 1160, 'PB.658': 1160, 'PB.6DH': 1160}
+        self.filiais()[11] = {'PB.620': 1160}
+        self.filiais()[12] = {'PB.620': 1160, 'PB.658': 1160, 'PB.6DH': 1160}
+        self.filiais()[13] = {'PB.620': 1250, 'PB.658': 1250, 'PB.6DH': 1250}
+
+    def filiais(self):
+        client = [15640, 725, 20347, 17644, 4168, 21254, 16906, 724, 8425, 15630, 766, 21933, 8944, 17984,
+                  16350, 1123, 1125, 1124, 10174, 7169, 6697, 4432, 156, 157, 155]
+        return client
 
     def abrir_arq(self):
         pass
@@ -73,7 +93,7 @@ class Parametros:
 
     def tab(self):
         tabela = {'Cgv':689, 'Avulsos':525}
-        if self.answer_pergunta() in tabela.keys():
+        if self.pergunta() in tabela.keys():
             return tabela.values()
 
     @staticmethod
