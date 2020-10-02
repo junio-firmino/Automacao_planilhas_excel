f8bc1a61244410826a405432acb39d1f1aa05579
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2020-10-01 23:03:25 -0300

    Inclusão de algumas classes.

--- Recebimento_pedidos.py
+++ Recebimento_pedidos.py
@@ -6,7 +6,21 @@ from selenium import webdriver
 class Abrir_Scd:
     caminho = input("Qual o caminho do arquivo? ")
     def __init__(self,site):
-        self.brower = webdriver.Firefox(executable_path=self.caminho)
-        self.brower.get("http://www.anp.gov.br/")
+        self.brower = webdriver.Firefox(executable_path = caminho)
+        self.brower.get(site)
         self.brower.implicitly_wait(5)
 
+class Abrir_NVC(Abrir_Scd):
+    def __init__(self,site):
+        super.__init__(site)
+
+class Fechar_NVC(Abrir_Scd):
+    def __init__(self,site):
+        super.__init__(site)
+
+class Create_path:
+    def path:
+        pass
+
+class Email:
+    pass
