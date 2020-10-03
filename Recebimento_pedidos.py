cf3906b8a40bf9ec844fbc85f691a7d7d3b4509a
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2020-10-02 21:33:46 -0300

    Mais classes para o escopo do projeto.

--- Recebimento_pedidos.py
+++ Recebimento_pedidos.py
@@ -4,17 +4,17 @@ from selenium import webdriver
 
 
 class Abrir_Scd:
-    caminho = input("Qual o caminho do arquivo? ")
     def __init__(self,site):
-        self.brower = webdriver.Firefox(executable_path = caminho)
+        self.caminho = input("Qual o caminho do arquivo? ")
+        self.brower = webdriver.Firefox(executable_path = self.caminho)
         self.brower.get(site)
         self.brower.implicitly_wait(5)
 
-class Abrir_NVC(Abrir_Scd):
+class Open_NVC(Abrir_Scd):
     def __init__(self,site):
         super.__init__(site)
 
-class Fechar_NVC(Abrir_Scd):
+class Close_NVC(Abrir_Scd):
     def __init__(self,site):
         super.__init__(site)
 
@@ -23,4 +23,16 @@ class Create_path:
         pass
 
 class Email:
+    pass
+
+class Choice_enginer:
+    pass
+
+class Enginer_open_NVC:
+    pass
+
+class Enginer_create_spreadsheet:
+    pass
+
+class Enginer_request_assent:
     pass
