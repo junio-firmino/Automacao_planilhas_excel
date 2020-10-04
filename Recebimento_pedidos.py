5bf5fafe04ab35057c1bdac0d693b1dc19c1d678
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2020-10-03 23:44:17 -0300

    Mais funcionalidades.
    Foi adicionado mais funcionalidades e feito alguns testes.

--- Recebimento_pedidos.py
+++ Recebimento_pedidos.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from openpyxl import load_workbook
 from selenium import webdriver
+import os
 
 
 class Abrir_Scd:
@@ -9,6 +10,7 @@ class Abrir_Scd:
         self.brower = webdriver.Firefox(executable_path = self.caminho)
         self.brower.get(site)
         self.brower.implicitly_wait(5)
+        self.brower.maximize_window()
 
 class Open_NVC(Abrir_Scd):
     def __init__(self,site):
@@ -19,14 +21,26 @@ class Close_NVC(Abrir_Scd):
         super.__init__(site)
 
 class Create_path:
-    def path:
+    def path(self):
         pass
 
 class Email:
     pass
 
 class Choice_enginer:
-    pass
+    def __init__(self):
+        self.escolha = input('Qual parte do processo de pedido você deseja executar?')
+
+    def engineer(self):
+        if self.escolha == '1':
+            return Enginer_open_NVC()
+
+        elif self.escolha == '2':
+            return Enginer_create_spreadsheet()
+
+        elif self.escolha == '3':
+            return Enginer_request_assent()
+
 
 class Enginer_open_NVC:
     pass
@@ -36,3 +50,6 @@ class Enginer_create_spreadsheet:
 
 class Enginer_request_assent:
     pass
+
+if __name__ == '__main__':
+    x=Abrir_Scd("http://www.anp.gov.br/")
