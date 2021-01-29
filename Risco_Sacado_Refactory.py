d153467ac41a0f5dd608440191d37a58fa04910a
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2021-01-28 22:37:49 -0300

    Try solve the problem

--- Risco_Sacado_Refactory.py
+++ Risco_Sacado_Refactory.py
@@ -3,7 +3,7 @@ import smtplib
 from email.message import EmailMessage
 from locale import setlocale, LC_ALL
 import assists
-from dateutil.relativedelta import relativedelta
+# from dateutil.relativedelta import relativedelta
 
 
 # I will refactory This project, for do it I chose the design pattern Facade.
@@ -49,10 +49,11 @@ class Planriscosacado:
         self.banco3 = banco
         self.info = Informationconstant()
         self.carencia = Carencia(valores=Cpgt(cpgt))
-        self.load = Loadworkbook()
+        #self.load = Loadworkbook()
+        self.chg = Change_loadworkbook()
 
     def plan_taxes(self):
-        aba_act = self.load.wb.active
+        aba_act = self.chg.wb.active
         for linha_plan in range(aba_act.max_row + 1, aba_act.max_row + 2):
             info = Distribuidoras().distri_cliente_polo_produto()[self.client0]
             for fili, info_1 in info.items():
@@ -105,14 +106,9 @@ class Planriscosacado:
 
 
 class Loadworkbook:
-    def __init__(self):  # trabalhar neste ponto
-        # if
-        self.wb = load_workbook(filename='risco_sacado(25.01.2021).xlsx')
-        # template_cpgt_risco_sacado_new.xlsx
-        self.wb_cpgt = load_workbook(filename='Cadastro_em_lote_RS(25.01.2021).xlsx')
-        # else:
-        # self.wb = load_workbook(filename='Risco Sacado - TMP(preço).xlsx') and
-        # self.wb_cpgt = load_workbook(filename='template_cpgt_risco_sacado_new.xlsx')+
+    def __init__(self):  # trabalhar neste ponto com a mudança na instanciação
+        self.wb = load_workbook(filename='Risco Sacado - TMP(preço).xlsx')
+        self.wb_cpgt = load_workbook(filename='template_cpgt_risco_sacado_new.xlsx')
 
     def open(self):
         return self.wb
@@ -131,6 +127,17 @@ class Loadworkbook:
 
     def close_wb_cpgt(self):
         self.wb_cpgt.close()
+
+
+class Change_loadworkbook:
+    def changing(self):
+        self.change = Loadworkbook()
+        if self.change.wb == load_workbook(filename='risco_sacado(25.01.2021).xlsx'):
+            self.change.wb = load_workbook(filename='risco_sacado(25.01.2021).xlsx')
+        if self.change.wb_cpgt == load_workbook(filename='Cadastro_em_lote_RS(25.01.2021).xlsx'):
+            self.change.wb_cpgt = load_workbook(filename='Cadastro_em_lote_RS(25.01.2021).xlsx')
+        else:
+           return self.change.wb and self.change.wb_cpgt
 
 
 class Listdistribuidora:
