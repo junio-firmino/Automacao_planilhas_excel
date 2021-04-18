b49adb8fdb2c013554eaac2b09e677d694ea35d4
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2021-04-17 23:46:54 -0300

    Short adjustment.

--- Risco_Sacado_Refactory.py
+++ Risco_Sacado_Refactory.py
@@ -3,6 +3,7 @@ import smtplib
 from email.message import EmailMessage
 from locale import setlocale, LC_ALL
 import assists
+import Var_ambiente
 
 
 # I will refactory This project, for do it I chose the design pattern Facade.
